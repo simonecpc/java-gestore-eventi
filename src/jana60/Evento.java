c8a916efe3c8437dae88fe380985c75fc42585f5
simonecpc <simone101199@gmail.com>
2022-07-01 10:02:11 +0200

    prima milestone completata

--- src/jana60/Evento.java
+++ src/jana60/Evento.java
@@ -1,5 +1,81 @@
 package jana60;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Evento {
 
+	private String titolo;
+	private LocalDate data;
+	private int numPostiTot;
+	private int numPostiPrenotati = 0;
+	private LocalDate oggi = LocalDate.now();
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	public Evento(String titolo, LocalDate data, int numPostiTot) throws Exception{
+		super();
+		this.titolo = titolo;
+		this.data = data;
+		this.numPostiTot = numPostiTot;
+		validaData();
+		validaPosti();
+		prenota();
+		disdici();
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public int getNumPostiPrenotati() {
+		return numPostiPrenotati;
+	}
+
+	public void setNumPostiPrenotati(int numPostiPrenotati) {
+		this.numPostiPrenotati = numPostiPrenotati;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public int getNumPostiTot() {
+		return numPostiTot;
+	}
+	
+	public void validaData() throws Exception {
+		if (oggi.isAfter(data)) {
+			throw new Exception ("Non puoi prenotare una vacanza nel passato, riprova.");
+		}
+	}
+	
+	public void validaPosti() throws Exception {
+		if (numPostiTot < 0) {
+			throw new Exception ("Inserire un numero positivo per la capienza");
+		}
+	}
+	
+	public int prenota() throws Exception {
+		if (numPostiPrenotati < 0) {
+			throw new Exception ("Inserire un numero positivo di posti prenotati");
+		}
+		return numPostiPrenotati + 1;
+	}
+	
+	public int disdici() throws Exception {
+		if (oggi.isAfter(data) || numPostiPrenotati <= 0){
+			throw new Exception ("La data è stata superata oppure i posti prenotati non sono pervenuti");
+		} 
+		return numPostiPrenotati - 1;
+	}
+
+	@Override
+	public String toString() {
+		return data + titolo;
+	}
+	
 }
